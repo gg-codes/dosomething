617d53884a538b17827651a5f74c77d844aab3e8
gaurav <gauravhrgandhi@gmail.com>
2018-07-30 09:17:28 +0530

    Added dynamic_programming_10

--- src/main/java/DynamicProgramming/dynamic_programming_10.java
+++ src/main/java/DynamicProgramming/dynamic_programming_10.java
@@ -18,13 +18,13 @@ public class dynamic_programming_10 {
         for (int[] subArr : arr) {
             Arrays.fill(subArr, -1);
         }
-        /*System.out.println(findBionomialFactorialDP(n, r) % modulo + ":" + count);
+        /*System.out.println(findBinomialFactorialDP(n, r) % modulo + ":" + count);
         count = 0;
-        System.out.println(findBionomialFactorial(n, r) + ":" + count);*/
-        System.out.println(findBionomialFactorialBottomUp(n, r));
+        System.out.println(findBinomialFactorial(n, r) + ":" + count);*/
+        System.out.println(findBinomialFactorialBottomUp(n, r));
     }
 
-    private static int findBionomialFactorialDP(int n, int r) throws Exception {
+    private static int findBinomialFactorialDP(int n, int r) throws Exception {
         count++;
         if (n < r) {
             return 0;
@@ -33,23 +33,23 @@ public class dynamic_programming_10 {
             return 1;
         }
         try {
-            arr[n - 1][r - 1] = arr[n - 1][r - 1] == -1 ? findBionomialFactorialDP(n - 1, r - 1) % modulo : arr[n - 1][r - 1];
-            arr[n - 1][r] = arr[n - 1][r] == -1 ? findBionomialFactorialDP(n - 1, r) % modulo : arr[n - 1][r];
+            arr[n - 1][r - 1] = arr[n - 1][r - 1] == -1 ? findBinomialFactorialDP(n - 1, r - 1) % modulo : arr[n - 1][r - 1];
+            arr[n - 1][r] = arr[n - 1][r] == -1 ? findBinomialFactorialDP(n - 1, r) % modulo : arr[n - 1][r];
         } catch (Exception e) {
             System.out.println(n + ":" + r);
         }
         return (arr[n - 1][r - 1] + arr[n - 1][r]) % modulo;
     }
 
-    private static int findBionomialFactorial(int n, int r) throws Exception {
+    private static int findBinomialFactorial(int n, int r) throws Exception {
         count++;
         if (n == r || r == 0) {
             return 1;
         }
-        return findBionomialFactorial(n - 1, r - 1) + findBionomialFactorial(n - 1, r);
+        return findBinomialFactorial(n - 1, r - 1) + findBinomialFactorial(n - 1, r);
     }
 
-    private static int findBionomialFactorialBottomUp(int n, int r) {
+    private static int findBinomialFactorialBottomUp(int n, int r) {
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= Math.min(i, r); j++) {
                 if (j == 0 || j == i) {
