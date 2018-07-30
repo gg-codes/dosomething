5b80906ef73723feabbaaaca59f74b6070cf71b1
gaurav <gauravhrgandhi@gmail.com>
2018-07-30 16:00:28 +0530

    Change dynamic_programming_10 to dynamic_programming_8, added dynamic_programming_10

--- src/main/java/DynamicProgramming/dynamic_programming_10.java
+++ src/main/java/DynamicProgramming/dynamic_programming_10.java
@@ -6,61 +6,45 @@ import java.util.Arrays;
  * Created by GAURAV on 7/30/2018.
  */
 public class dynamic_programming_10 {
-    static int modulo = (int) (Math.pow(10, 9) + 7);
-    ;
-    static int count;
-    static int[][] arr;
+    static int[][] dpArr;
+    static int capacity;
 
-    public static void main(String[] args) throws Exception {
-        int n = 5;
-        int r = 2;
-        arr = new int[n + 1][r + 1];
-        for (int[] subArr : arr) {
-            Arrays.fill(subArr, -1);
+    public static void main(String[] args) {
+        int val[] = new int[]{55, 61, 51, 75, 17, 22, 4, 13, 39, 28, 77, 49, 46, 91, 14, 67, 88, 62, 25, 37, 69, 38, 59, 62, 48, 88, 100, 53};
+        int wt[] = new int[]{96, 16, 34, 53, 88, 6, 50, 26, 76, 10, 8, 4, 37, 18, 73, 54, 30, 31, 97, 2, 28, 24, 2, 30, 79, 77, 33, 86};
+        int W = 83;
+        int n = val.length;
+        dpArr = new int[val.length + 1][W + 1];
+        for (int[] arr : dpArr) {
+            Arrays.fill(arr, -1);
         }
-        /*System.out.println(findBinomialFactorialDP(n, r) % modulo + ":" + count);
-        count = 0;
-        System.out.println(findBinomialFactorial(n, r) + ":" + count);*/
-        System.out.println(findBinomialFactorialBottomUp(n, r));
+        System.out.println(knapSack(W, wt, val, n));
+        System.out.println(capacity);
     }
 
-    private static int findBinomialFactorialDP(int n, int r) throws Exception {
-        count++;
-        if (n < r) {
+    private static int knapSack(int maxCapacity, int[] wtArr, int[] valArr, int valIndex) {
+//        System.out.println(valIndex + ":" + maxCapacity);
+        try {
+            if (dpArr[valIndex][maxCapacity] != -1) {
+                return dpArr[valIndex][maxCapacity];
+            }
+        } catch (Exception e) {
+            System.out.println(valIndex + ":" + maxCapacity);
+            throw e;
+        }
+        capacity++;
+
+        if (maxCapacity == 0 || valIndex == 0) {
+            dpArr[valIndex][maxCapacity] = 0;
             return 0;
         }
-        if (n == r || r == 0) {
-            return 1;
+        if (wtArr[valIndex - 1] > maxCapacity) {
+            return knapSack(maxCapacity, wtArr, valArr, valIndex - 1);
         }
-        try {
-            arr[n - 1][r - 1] = arr[n - 1][r - 1] == -1 ? findBinomialFactorialDP(n - 1, r - 1) % modulo : arr[n - 1][r - 1];
-            arr[n - 1][r] = arr[n - 1][r] == -1 ? findBinomialFactorialDP(n - 1, r) % modulo : arr[n - 1][r];
-        } catch (Exception e) {
-            System.out.println(n + ":" + r);
-        }
-        return (arr[n - 1][r - 1] + arr[n - 1][r]) % modulo;
-    }
 
-    private static int findBinomialFactorial(int n, int r) throws Exception {
-        count++;
-        if (n == r || r == 0) {
-            return 1;
-        }
-        return findBinomialFactorial(n - 1, r - 1) + findBinomialFactorial(n - 1, r);
-    }
-
-    private static int findBinomialFactorialBottomUp(int n, int r) {
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= Math.min(i, r); j++) {
-                if (j == 0 || j == i) {
-                    arr[i][j] = 1;
-                } else {
-                    arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j]) % modulo;
-                }
-            }
-        }
-        return arr[n][r] % modulo;
+        int included = knapSack(maxCapacity, wtArr, valArr, valIndex - 1);
+        int notIncluded = valArr[valIndex - 1] + knapSack(maxCapacity - wtArr[valIndex - 1], wtArr, valArr, valIndex - 1);
+        dpArr[valIndex][maxCapacity] = Math.max(notIncluded, included);
+        return dpArr[valIndex][maxCapacity];
     }
 }
-
-
