44cd321bc210439ce4220f3d5ca6408972273202
gaurav <gauravhrgandhi@gmail.com>
2018-09-19 07:35:50 +0530

    Complete KSumPath

--- src/main/java/tag/amazon/KSumPath.java
+++ src/main/java/tag/amazon/KSumPath.java
@@ -3,9 +3,7 @@ package tag.amazon;
 import binarytree.Node;
 import binarytree.TreeUtilities;
 
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 import static binarytree.PrintBinaryTree.printBinaryTree;
 
@@ -13,28 +11,44 @@ import static binarytree.PrintBinaryTree.printBinaryTree;
  * Created by GAURAV on 9/19/2018.
  */
 public class KSumPath {
+
+    private int requiredSum = 5;
+    private int pathCount = 0;
+
     public static void main(String[] args) {
-        Node node = TreeUtilities.generateTree("1 3 L 3 2 L 3 -1 R -1 1 R");
+        Node node = TreeUtilities.generateTree("1 3 L 3 2 L 3 1 R");
         printBinaryTree(node);
-        findKSumPath(node);
+        new KSumPath().findKSumPath(node);
     }
 
-    private static void findKSumPath(Node node) {
-        List<Integer> paths = new LinkedList<>();
+    private void findKSumPath(Node node) {
+        LinkedList<Integer> paths = new LinkedList<>();
         kSumPathUtil(node, paths);
     }
 
-    private static void kSumPathUtil(Node node, List<Integer> paths) {
+    private void kSumPathUtil(Node node, LinkedList<Integer> paths) {
         if (node == null) {
             return;
         }
         paths.add(node.data);
         kSumPathUtil(node.left, paths);
         kSumPathUtil(node.right, paths);
-        System.out.println("Path for " + node.data + " is: " + Arrays.toString(paths.toArray()));
-        /*for (int path : paths) {
-            System.out.print(path + ",");
-        }*/
-        paths.remove(node.data);
+//        System.out.println("Path for " + node.data + " is: " + Arrays.toString(paths.toArray()));
+        int sum = 0;
+        for (int path = paths.size() - 1; path > -1; path--) {
+            sum += paths.get(path);
+            if (sum == requiredSum) {
+//                printString(paths, path);
+                pathCount++;
+            }
+        }
+        paths.pollLast();
+    }
+
+    private void printString(LinkedList<Integer> paths, int path) {
+        while (path != paths.size()) {
+            System.out.print(paths.get(path++) + " ");
+        }
+        System.out.println("");
     }
 }
